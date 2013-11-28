5941d9319b504b91483f7de1cfafc6a4eb61bcdb
Rio Nishino <n.rio1281@gmail.com>
2013-11-28 22:39:32 +0900

    Imprement create method for login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,13 @@ class SessionsController < ApplicationController
 
   # POST /session
   def create
+  	user = User.find_by_name(params[:name])
+  	if user and user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		# TODO: redirect to the admin page
+  	else
+  		redirect_to new_session_path, alert: "ユーザー名かパスワードが間違っています"
+  	end
   end
 
   # DELETE /session
