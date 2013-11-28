bbc84ad3ced86f95bc0c7df14fb2759aa4b73a60
Rio Nishino <n.rio1281@gmail.com>
2013-11-28 21:39:20 +0900

    Rename users.password to users.password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131128122330) do
+ActiveRecord::Schema.define(version: 20131128123528) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
