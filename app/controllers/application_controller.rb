2279bb7ce9988f687971674ea5f809b82bb98eb6
Rio Nishino <n.rio1281@gmail.com>
2013-11-30 21:50:50 +0900

    Implement authenticate_user! before_action
    ログインしていないとアクセス出来ないページのためのbefore_actionを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def authenticate_user!
+  	redirect_to new_session_path unless current_user
+  end
+
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,6 @@
 class SitesController < ApplicationController
   before_action :set_site, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create]
 
   # GET /sites
   # GET /sites.json
