43b1da5f0f6a0dd88425c773fb79e29fc3140ea4
Rio Nishino <n.rio1281@gmail.com>
2013-11-30 21:11:51 +0900

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Blog::Application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users
 
+  root 'sites#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
